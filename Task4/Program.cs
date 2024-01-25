19371122c69d27733b37edd2189bcf5813c489e6
Yura <aderevs@gmail.com>
2024-01-25 17:15:28 +0200

    realised task4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TripleDictionary ukrainianEnglishGermar = new TripleDictionary("Українаська", "English", "Deutsch");
+            ukrainianEnglishGermar.AddWord("дерево", "tree", "Baum");
+            ukrainianEnglishGermar.AddWord("стіл", "table", "Tabelle");
+            ukrainianEnglishGermar.AddWord("книга", "book", "Buch");
+            ukrainianEnglishGermar.AddWord("сковорода", "pan", "Bratpfanne");
+            ukrainianEnglishGermar.AddWord("олівець", "pensil", "Bleistift");
+            ukrainianEnglishGermar.AddWord("вулиця", "street", "Straße");
+            ukrainianEnglishGermar.AddWord("яблуко", "apple", "Apfel");
+            ukrainianEnglishGermar.AddWord("продукти", "grocery", "Produkte");
+            ukrainianEnglishGermar.AddWord("приклад", "example", "Beispiel");
+
+            Console.WriteLine("enter your ukrainian word:");
+            string word = Console.ReadLine();
+            Console.WriteLine($"English translate: {ukrainianEnglishGermar.GetLanguage1Translate(word)}");
+            Console.WriteLine($"Deutsch translate: {ukrainianEnglishGermar.GetLanguage2Translate(word)}");
+            Console.WriteLine("\nAll added words:");
+            foreach (var item in ukrainianEnglishGermar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Task4/TripleDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class TripleDictionary : IEnumerable<KeyValuePair<string, Tuple<string, string>>>
+    {
+        private Dictionary<string, Tuple<string, string>> _dictionary;
+
+        public TripleDictionary()
+        {
+            _dictionary = new Dictionary<string, Tuple<string, string>>();
+        }
+        public TripleDictionary(string mainLanguage, string firstTranslateLanguage, string secondTranslateLanguage)
+        {
+            _dictionary = new Dictionary<string, Tuple<string, string>>();
+            AddWord(mainLanguage, firstTranslateLanguage, secondTranslateLanguage);
+        }
+        public void AddWord(string mainLanguageWord, string firstLanguageTranslate, string secondLanguageTranslate)
+        {
+            _dictionary.Add(mainLanguageWord, new Tuple<string, string>(firstLanguageTranslate, secondLanguageTranslate));
+        }
+        public string GetLanguage1Translate(string ukrainianWord)
+        {
+            return _dictionary[ukrainianWord].Item1;
+        }
+        public string GetLanguage2Translate(string ukrainianWord)
+        {
+            return _dictionary[ukrainianWord].Item2;
+        }
+
+        //sorting by main language words
+        public void Sort()
+        {
+
+        }
+
+        public IEnumerator<KeyValuePair<string, Tuple<string, string>>> GetEnumerator()
+        {
+            foreach (var item in _dictionary)
+            {
+                yield return new KeyValuePair<string, Tuple<string, string>>(item.Key, new Tuple<string, string>(item.Value.Item1, item.Value.Item2));
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            GetEnumerator();
+            yield break;
+        }
+
+    }
+}
