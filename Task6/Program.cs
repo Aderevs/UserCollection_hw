cbda8b012fa75348673b4bfbd359c4c554f714cb
Yura <aderevs@gmail.com>
2024-01-25 17:33:25 +0200

    realised task6

---
+++ Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static IEnumerable<int> GetSquaresOfOddNumbers(int[] array)
+        {
+            foreach (var i in array)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return i * i;
+                }
+            }
+            yield break;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = GetSquaresOfOddNumbers(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }).ToList();
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
